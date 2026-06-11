bfa6d340617ee7769f423a86960974a35ef27355
victhipe <75435945+victhipe@users.noreply.github.com>
2026-06-11 16:19:19 +0400

    .

--- backend/Review.js
+++
@@ -1,8 +0,0 @@
-const {Schema, model} = require('mongoose');
-
-const reviewSchema = new Schema({
-    login: String,
-    text: {type: String}
-})
-
-module.exports = model("Review",reviewSchema, "Review" );
